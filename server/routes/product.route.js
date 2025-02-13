30621483baf5253dcf2512852fdb29f99843e7ce
CharlesKibet <charlesbiegon973@gmail.com>
2025-02-13 19:09:27 +0300

    setting up routes for getting products done

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,10 +1,21 @@
 //Setting up the routes
 import express from 'express';
 
-import { createProduct } from '../controllers/product.controller.js';
+import {
+  createProduct,
+  getProduct,
+  getProducts,
+} from '../controllers/product.controller.js';
 
 const router = express.Router();
 
+//Route for adding/creatin g a product
 router.post('/', createProduct);
+
+//Router for getting one product
+router.get('/:id', getProduct);
+
+//Router for getting all products
+router.get('/', getProducts);
 
 export default router;
