bb224a1eec14cdf3b52df087ffdc35f472179204
CharlesKibet <charlesbiegon973@gmail.com>
2025-02-13 15:22:40 +0300

    connection set up
    Connecting our server to the mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,19 @@
 //Entry point for our api
 import express from 'express';
 
+import { connectDB } from './config/db.js';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
+//
+app.get('/', (req, res) => {
+  res.send('Hello from express');
+});
 
 app.listen(5000, () => {
+  connectDB();
   console.log('Server started at http://localhost:5000');
 });
